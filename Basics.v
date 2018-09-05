1d90f122194cb407fbd4bc2acd741f7267bd0ae7
Bianca <binaksrc@gmail.com>
2018-09-04 23:49:25 -0300

    update

--- Basics.v
+++ Basics.v
@@ -390,6 +390,20 @@ Proof.
   reflexivity.
 Qed.
 
+Theorem negation_fn_applied_twice :
+  forall (f:bool -> bool),
+  (forall (x:bool), f x = negb x) ->
+  forall (b:bool), f (f b) = b.
+Proof.
+  intros f.
+  intros H.
+  intros b.
+  rewrite -> H.
+  rewrite -> H.
+  rewrite -> negb_involutive.
+  reflexivity.
+Qed.
+
 
 
 
