b3328ac96b17020aee52e39e1a41b9f5df551e27
Bianca <binaksrc@gmail.com>
2018-09-04 23:37:58 -0300

    update

--- Basics.v
+++ Basics.v
@@ -377,6 +377,19 @@ Proof.
   - simpl. reflexivity.
 Qed.
 
+Theorem identity_fn_applied_twice :
+  forall (f:bool -> bool),
+  (forall (x:bool), f x = x) ->
+  forall (b:bool), f (f b) = b.
+Proof.
+  intros f.
+  intros H.
+  intros b.
+  rewrite -> H.
+  rewrite -> H.
+  reflexivity.
+Qed.
+
 
 
 
