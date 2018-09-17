e781a2881afc645b9e62ff0d81183f4e66f64b49
Bianca Rodrigues <binaksrc@gmail.com>
2018-09-17 20:13:54 -0300

    update Induction.v

--- Induction.v
+++ Induction.v
@@ -23,4 +23,56 @@ Proof.
   - simpl. rewrite -> IHn'.
     reflexivity. Qed.
 
+Theorem plus_n_Sm : forall n m : nat,
+  S (n + m) = n + (S m).
+Proof.
+  intros n m. induction n as [| n' IHn'].
+  - simpl. reflexivity.
+  - simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+Theorem plus_comm : forall n m : nat,
+  n + m = m + n.
+Proof.
+  intros n m. induction n as [| n' IHn'].
+  - simpl. rewrite <- plus_n_O.
+    reflexivity.
+  - simpl. rewrite -> IHn'.
+    rewrite <- plus_n_Sm.
+    reflexivity.
+Qed.
+
+Theorem plus_assoc : forall n m p : nat,
+  n + (m + p) = (n + m) + p.
+Proof.
+  intros n m p. induction n as [| n' IHn'].
+  - simpl. reflexivity.
+  - simpl. rewrite -> IHn'.
+    reflexivity.
+Qed.
+
+Fixpoint double (n:nat) :=
+  match n with
+  | O => O
+  | S n' => S (S (double n'))
+  end.
+
+Lemma double_plus : forall n, double n = n + n.
+Proof.
+  intros n. induction n as [| n' IHn'].
+  - simpl. reflexivity.
+  - simpl. rewrite -> IHn'.
+    rewrite -> plus_n_Sm.
+    reflexivity.
+Qed.
+
+Theorem evenb_S : forall n:nat,
+  evenb (S n) = negb (evenb n).
+
+
+
+
+
+
+
 
