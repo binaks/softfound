5973af9ce1a9e45894528d65df8d568e5c1d7044
Bianca Rodrigues <binaksrc@gmail.com>
2018-09-17 19:19:24 -0300

    add Induction.v

---
+++ Induction.v
@@ -0,0 +1,16 @@
+Require Export Basics.
+
+Theorem plus_n_O : forall n:nat, n = n + 0.
+Proof.
+  intros n. induction n as [| n' IHn'].
+  - reflexivity.
+  - simpl. rewrite <- IHn'. reflexivity.
+
+Theorem minus_diag : forall n,
+  minus n n = 0.
+Proof.
+  intros n. induction n as [| n' IHn'].
+  - simpl. reflexivity.
+  - simpl. rewrite -> IHn'.
+    reflexivity. Qed.
+
