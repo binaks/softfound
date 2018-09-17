151933bacec04d630fb0dc25288d3d1cf721f0d7
Bianca Rodrigues <binaksrc@gmail.com>
2018-09-17 19:23:05 -0300

    update Induction.v

--- Induction.v
+++ Induction.v
@@ -5,6 +5,7 @@ Proof.
   intros n. induction n as [| n' IHn'].
   - reflexivity.
   - simpl. rewrite <- IHn'. reflexivity.
+Qed.
 
 Theorem minus_diag : forall n,
   minus n n = 0.
@@ -13,4 +14,13 @@ Proof.
   - simpl. reflexivity.
   - simpl. rewrite -> IHn'.
     reflexivity. Qed.
+
+Theorem mult_0_r : forall n:nat,
+  n * 0 = 0.
+Proof.
+  intros n. induction n as [| n' IHn'].
+  - simpl. reflexivity.
+  - simpl. rewrite -> IHn'.
+    reflexivity. Qed.
+
 
