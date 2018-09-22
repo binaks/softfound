2b1adcf0c83849b5ae7e63426c6c58dc5aaaffb2
Bianca <binaksrc@gmail.com>
2018-09-22 03:03:21 -0300

    update Lists.v

--- Lists.v
+++ Lists.v
@@ -90,6 +90,138 @@ Fixpoint length (l:natlist) : nat :=
   | h :: t => S (length t)
   end.
 
+Fixpoint app (l_1 l_2 : natlist) : natlist :=
+  match l_1 with
+  | nil => l_2
+  | h :: t => h :: (app t l_2)
+  end.
+
+Notation "x ++ y" := (app x y)
+                     (right associativity, at level 60).
+
+Example test_appl: [1;2;3] ++ [4;5] = [1;2;3;4;5].
+Proof. reflexivity. Qed.
+Example test_app2: nil ++ [4;5] = [4;5].
+Proof. reflexivity. Qed.
+Example test_app3: [1;2;3] ++ nil = [1;2;3].
+Proof. reflexivity. Qed.
+
+Definition hd (default:nat) (l:natlist) : nat :=
+  match l with
+  | nil => default
+  | h :: t => h
+  end.
+
+Definition t1 (l:natlist) : natlist :=
+  match l with
+  | nil => nil
+  | h :: t => t
+  end.
+
+Example test_hd_1: hd 0 [1;2;3] = 1.
+Proof. reflexivity. Qed.
+Example test_hd_2: hd 0 [] = 0.
+Proof. reflexivity. Qed.
+Example test_t1: t1 [1;2;3] = [2;3].
+Proof. reflexivity. Qed.
+
+Fixpoint nonzeros (l:natlist) : natlist :=
+  match l with
+  | nil => nil
+  | h :: t => match beq_nat h 0 with
+              | true => nonzeros t
+              | false => h :: (nonzeros t)
+              end
+end.
+
+Example test_nonzeros:
+  nonzeros [0;1;0;2;3;0;0] = [1;2;3].
+Proof. reflexivity. Qed.
+
+Fixpoint oddmembers (l:natlist) : natlist :=
+  match l with
+  | nil => nil
+  | h :: t => match evenb h with
+              | true => oddmembers t
+              | false => h :: (oddmembers t)
+              end
+end.
+
+Example test_oddmembers:
+  oddmembers [0;1;0;2;3;0;0] = [1;3].
+Proof. reflexivity. Qed.
+
+Definition countoddmembers (l:natlist) : nat :=
+  length (oddmembers l).
+
+Example test_countoddmembers1:
+  countoddmembers [1;0;3;1;4;5] = 4.
+Proof. reflexivity. Qed.
+
+Example test_countoddnumbers2:
+  countoddmembers [0;2;4] = 0.
+Proof. reflexivity. Qed.
+
+Example test_countoddnumbers3:
+  countoddmembers nil = 0.
+Proof. reflexivity. Qed.
+
+Fixpoint alternate (l_1 l_2 : natlist) : natlist :=
+  match l_1 with
+  | nil => l_2
+  | h :: t => match l_2 with
+              | nil => l_1
+              | h' :: t' => h :: h' :: (alternate t t')
+              end
+end.
+
+Example test_alternate1:
+  alternate [1;2;3] [4;5;6] = [1;4;2;5;3;6].
+Proof. reflexivity. Qed.
+
+Example test_alternate2:
+  alternate [1] [4;5;6] = [1;4;5;6].
+Proof. reflexivity. Qed.
+
+Example test_alternate3:
+  alternate [1;2;3] [4] = [1;4;2;3].
+Proof. reflexivity. Qed.
+
+Example test_alternate4:
+  alternate [] [20;30] = [20;30].
+Proof. reflexivity. Qed.
+
+Definition bag := natlist.
+
+Fixpoint count (v:nat) (s:bag) : nat :=
+  match s with
+  | nil => 0
+  | h :: t => match beq_nat h v with
+              | true => S (count v t)
+              | false => count v t
+              end
+  end.
+
+Example test_count1: count 1 [1;2;3;1;4;1] = 3.
+Proof. reflexivity. Qed.
+
+Example test_count2: count 6 [1;2;3;1;4;1] = 0.
+Proof. reflexivity. Qed.
+
+Definition sum : bag -> bag -> bag :=
+  app.
+
+Example test_sum_1: count 1 (sum [1;2;3] [1;4;1]) = 3.
+Proof. reflexivity. Qed.
+
+
+
+
+
+
+
+
+
 
 
 
